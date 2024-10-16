62951e937299dffd36ee42e9b61d8ce758cdfe50
rubem007 <rubemnascimento81@gmail.com>
2024-10-16 19:42:28 +0200

    add ansible conf

--- ansible/docker-install.sh
+++ ansible/docker-install.sh
@@ -5,7 +5,7 @@ sudo apt-get update && sudo apt-get install ca-certificates curl gnupg --yes
 
 # Adicionando a chave GPG do Docker e adicionando o repositório
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
