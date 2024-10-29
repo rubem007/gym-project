c5a3ef80af51082c3c6bfb73a411d4b814b55ecd
rubem007 <rubemnascimento81@gmail.com>
2024-10-29 16:31:56 +0100

    add sonarqube

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -28,7 +28,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "20.8.5"
   cluster_name    = "eks-cluster"
-  cluster_version = "1.29"
+  cluster_version = "1.31"
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
