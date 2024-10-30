b2dc311f3e98796dec7a78041c12fb352582f451
rubem007 <rubemnascimento81@gmail.com>
2024-10-30 14:00:31 +0100

    update dto

--- backoffice-api/src/customer/customer.controller.ts
+++ backoffice-api/src/customer/customer.controller.ts
@@ -9,8 +9,8 @@ import {
   Query,
 } from '@nestjs/common';
 import { CustomerService } from './customer.service';
-import { CreateCustomerDto } from './dto/create-customer.dto';
-import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { CreateCustomerDto } from './models/dto/create-customer.dto';
+import { UpdateCustomerDto } from './models/dto/update-customer.dto';
 
 @Controller('customer')
 export class CustomerController {
