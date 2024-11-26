56e9ca3f61e26c6aa18144b6fb4c0f42fd84eabe
rubem007 <rubemnascimento81@gmail.com>
2024-11-26 14:25:48 +0100

    update app

--- backoffice-api/src/app.module.ts
+++ backoffice-api/src/app.module.ts
@@ -2,15 +2,12 @@ import { Module } from '@nestjs/common';
 import { CustomerModule } from './customer/customer.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { PlanModule } from './plan/plan.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [
-    /*CustomerModule, PrismaModule, PlanModule*/
-  ],
+  imports: [CustomerModule, PrismaModule],
   controllers: [AppController],
-  providers: [/*PrismaService,*/ AppService],
+  providers: [PrismaService, AppService],
 })
 export class AppModule {}
