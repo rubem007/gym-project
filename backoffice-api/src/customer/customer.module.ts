8a0dbd2ad49251eb2d9d3ee67026c1000612c317
rubem007 <rubemnascimento81@gmail.com>
2024-11-26 14:29:51 +0100

    update path

--- backoffice-api/src/customer/customer.module.ts
+++ backoffice-api/src/customer/customer.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
